6cb34fbf8e8af3beac07edb6a21cdde43ef1e096
Hanna Parhim <anna.parhimovich@yandex.ru>
2026-05-11 00:50:11 +0300

    Временно отключаем фоновые задачи и WebSocket для запуска

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 import http from "node:http";
 import app from "./app.js";
 import config from "./config/index.js";
-import { bootstrapBackgroundJobs } from "./services/jobBootstrap.js";
-import { initRealtime } from "./services/realtimeService.js";
+// import { bootstrapBackgroundJobs } from "./services/jobBootstrap.js";
+// import { initRealtime } from "./services/realtimeService.js";
 
 const server = http.createServer(app);
-initRealtime(server);
-
-bootstrapBackgroundJobs();
+// initRealtime(server);
+// bootstrapBackgroundJobs();
 
 const PORT = process.env.PORT || config.port || 4000;
 server.listen(PORT, () => {
